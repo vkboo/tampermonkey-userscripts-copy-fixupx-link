cbce3d1ad0d8993f5c8adfa682cd5d9585176e1d
bo.zhao <bo.zhao@iglooinsure.com>
2024-10-31 10:30:32 +0800

    chore: v1.0.1 release

--- dist/react-userscripts.user.js
+++ dist/react-userscripts.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name     FixupX share link
 // @namespace https://github.com/vkboo/tampermonkey-userscripts-copy-fixupx-link
-// @version  1.0
+// @version  1.0.1
 // @description Copy FixupX link when click share button in X.com
 // @include https://x.com
 // @include https://x.com/*
